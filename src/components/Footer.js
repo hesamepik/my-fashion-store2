0ceebf6e3cf97d40f4ada0df46545d08d6a14aae
hesam <hesamepik@gmail.com>
2025-06-28 23:45:33 +0330

    شروع دوباره ریپازیتوری

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <footer className="mb-5">
+        <div className="h-8 flex justify-center items-center">
+          <p className="mt-5 text-[10px] tracking-widest text-gray-400 font-semibold">
+            ساخته شده با عشق توسط hesamepik
+          </p>
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
